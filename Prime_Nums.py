5b7194a455e1f135916f75f3e2d4844f55789ce7
LostandAdrift <73628382+LostandAdrift@users.noreply.github.com>
2022-02-25 22:12:50 -0800

    Update Prime_Nums.py

--- Prime_Nums.py
+++ Prime_Nums.py
@@ -1,8 +1,10 @@
 def find_primes (self):
     max_num = self
     prime_nums = []
+    # This creates a list of integers from 2 to the number called.
     for i in range(2,max_num):
         prime_nums.append(i)
+   # This iterates through the list, removing every number divisible by it. The only numbers remaining will be primes.   
     for num in prime_nums:
         for x in range(num+1,max_num):
             if x % num == 0:
